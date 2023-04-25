bead8489297a5b27d13aec4c2c9bd3c02e8f99f1
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-25 09:08:22 -0700

    Set up basic RESTful routes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    return res.send('Recieved a GET HTTP method');
+})
+
+app.post('/', (req, res) => {
+    return res.send('Recieved a POST HTTP method');
+})
+
+app.put('/', (req, res) => {
+    return res.send('Recieved a PUT HTTP method');
+})
+
+app.delete('/', (req, res) => {
+    return res.send('Recieved a DELETE HTTP method');
 })
 
 app.listen(process.env.PORT, () => 
