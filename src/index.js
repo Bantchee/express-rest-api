4f5628ad31298f51b88beb3c61ab77db003d0ac3
Jar'Zeno <jarzenojarrett@gmail.com>
2023-04-25 11:42:42 -0700

    Complete REST API tutorial

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import {v4 as uuidv4 } from 'uuid';
+import models from './models';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// This allows all origins to access this server (Use white list of production apps)
 app.use(cors());
 
-app.get('/', (req, res) => {
-    return res.send('Recieved a GET HTTP method');
-})
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    };
+    next();
+});
 
-app.post('/', (req, res) => {
-    return res.send('Recieved a POST HTTP method');
-})
-
-app.put('/', (req, res) => {
-    return res.send('Recieved a PUT HTTP method');
-})
-
-app.delete('/', (req, res) => {
-    return res.send('Recieved a DELETE HTTP method');
-})
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () => 
     console.log(`Example app listening on http://localhost:${process.env.PORT}/`));
